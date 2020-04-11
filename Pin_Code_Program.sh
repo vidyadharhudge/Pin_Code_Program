3fc20770749df682ac77f5979ee28aa7bdf73108
vidyadharhudge <vidyadharhudge1997@gmail.com>
2020-04-11 14:37:25 +0530

    Added:-Pattern For Valid Pincode

--- Pin_Code_Program.sh
+++ Pin_Code_Program.sh
@@ -1,2 +1,20 @@
 #!/bin/bash -x
 echo "Welcome To Pin Code Validation Program"
+
+#CONSTANTS
+PINCODEPATTERN="^[1-9][0-9]{5}$"
+
+#function for check pattern
+function Check_Pattern()
+{
+   if [[ $1 =~ $2 ]]
+   then
+        echo "Valid Pattern."
+   else
+        echo "Invalid Pattern."
+   fi
+}
+
+#user input and passing the parameter
+read -p "Enter The Pincode" Pincode
+Check_Pattern $Pincode $PINCODEPATTERN
